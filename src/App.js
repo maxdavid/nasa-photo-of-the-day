92d0f7b0512c28570e0e3cbaed82326988a11dcf
maxdavid <me@maxdavid.com>
2019-07-10 14:21:05 -0700

    Basic image fetching/displaying using Hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.scss';
+
+import { Image, Description } from './components';
 
 function App() {
+  const [apod, setApod] = useState({});
+
+  const fetchApod = () => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-07-22`
+      )
+      .then(res => setApod(res.data))
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => fetchApod(), []);
+
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+    <div className='App'>
+      <Image url={apod.hdurl} alt={apod.title} />
+      <Description
+        title={apod.title}
+        date={apod.date}
+        explanation={apod.explanation}
+      />
     </div>
   );
 }
